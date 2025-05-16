8347a7d89f393270148a7170d6c5ccdb1d7aebbd
LeeJaeHyuk7971 <ericking7972@gmail.com>
2025-05-17 00:06:42 +0900

    머지 오류 해결

--- app/src/main/java/com/please/di/NetworkModule.kt
+++ app/src/main/java/com/please/di/NetworkModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 //현재 elb로 다이렉트 접근. api gateway스킵
 //const val BASE_URL = "https://2y3az5fho1.execute-api.ap-northeast-2.amazonaws.com/Subtrack-stage/"
 //const val BASE_URL = "http://43.200.131.230:3000/"
-const val  BASE_URL = "https://vw0y369jm5.execute-api.ap-northeast-2.amazonaws.com/Subtrack/"
+const val  BASE_URL = "http://web-alb-subtrack-462963304.ap-northeast-2.elb.amazonaws.com/"
 
 
 @Module
